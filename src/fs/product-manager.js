6a24572c4fc74bbbc3de91016c8f1fefa572765c
Estanislao <alvarof260@gmail.com>
2023-12-16 12:23:32 -0300

    feat: add get product in class

--- src/fs/product-manager.js
+++ src/fs/product-manager.js
@@ -13,4 +13,19 @@ class ProductManager {
       await writeFile(this.#path, JSON.stringify([], null, '\t'))
     }
   }
+
+  getProduct = async () => {
+    try {
+      const response = await readFile(this.#path, 'utf-8')
+      const products = JSON.parse(response)
+      return products
+    } catch (err) {
+      console.error('Error al leer el archivo', err)
+    }
+  }
 }
+(async () => {
+  const dm = new ProductManager('src/data/products.json')
+  const products = await dm.getProduct()
+  console.log(products)
+})()

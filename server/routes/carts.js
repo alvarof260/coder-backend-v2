e9f977939cfbab17a575a02165814c1950ccb85a
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-01-05 19:10:33 -0300

    feat: add the PUT method to api/carts with mongoDB

--- server/routes/carts.js
+++ server/routes/carts.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
+import { Types } from 'mongoose'
 import { cartsModel } from '../dao/models/carts.js'
+import { productsModel } from '../dao/models/products.js'
 // import { CartManager } from '../dao/fs/cart-manager.js'
 
 const router = Router()
@@ -8,7 +10,7 @@ const router = Router()
 // crear el carrito
 router.post('/', async (req, res) => {
   try {
-    const cart = await cartsModel.create()
+    const cart = await cartsModel.create({})
     res.status(201).json({ status: 'success', payload: cart })
   } catch (err) {
     res.status(500).json({ status: 'error', error: err.message })
@@ -41,7 +43,23 @@ router.get('/:cid([a-fA-F0-9]{24})', async (req, res) => {
 })
 
 // Actualizar los productos que estan en el carrito
-router.put('/:cid((\\d+))/product/:pid((\\d+))', async (req, res) => {
+router.put('/:cid([a-fA-F0-9]{24})/product/:pid([a-fA-F0-9]{24})', async (req, res) => {
+  try {
+    const cid = req.params.cid
+    const pid = req.params.pid
+    const exists = await productsModel.exists({ _id: pid })
+    if (!exists) return res.status(404).json({ status: 'error', error: 'Product not found.' })
+    const cartToAdd = await cartsModel.findById(cid)
+    if (cartToAdd === null) return res.status(404).json({ status: 'error', error: 'Cart not found.' })
+    const prodIndex = cartToAdd.products.findIndex(el => el.product == pid)
+    console.log(prodIndex)
+    console.log(cartToAdd.products)
+    prodIndex > -1 ? cartToAdd.products[prodIndex].quantity += 1 : cartToAdd.products.push({ product: new Types.ObjectId(pid), quantity: 1 })
+    const cartUpdated = await cartsModel.findByIdAndUpdate(cid, cartToAdd, { returnDocument: 'after' })
+    res.status(200).json({ status: 'success', payload: cartUpdated })
+  } catch (err) {
+    res.status(500).json({ status: 'error', error: err.message })
+  }
 /*   try {
     const cid = parseInt(req.params.cid)
     const pid = parseInt(req.params.pid)

c5711757063bfa85b6e223b088e9b1fa6ee1146e
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-01-11 15:29:53 -0300

    feat: add a login of users

--- server/routes/session.js
+++ server/routes/session.js
@@ -3,11 +3,34 @@ import { userModel } from '../dao/models/user.js'
 
 const router = Router()
 
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      return res.status(400).json({ status: 'error', error: 'Email and password are not optional.' })
+    }
+    const user = await userModel.findOne({ email })
+    if (!user) {
+      return res.status(404).json({ status: 'error', error: `User with email: ${email} not found.` })
+    }
+    if (user.password !== password) return res.status(400).json({ status: 'error', error: 'The password is incorrect, try again.' })
+    req.session.user = {
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      age: user.age
+    }
+    res.redirect('/products')
+  } catch (err) {
+    res.status(500).json({ status: 'error', error: err.message })
+  }
+})
+
 router.post('/register', async (req, res) => {
   try {
     const { email, password } = req.body
     if (!email || !password) {
-      return res.status(400).json({ status: 'error', error: 'Email and password are not optional' })
+      return res.status(400).json({ status: 'error', error: 'Email and password are not optional.' })
     }
     const user = await userModel.findOne({ email })
     if (user) {

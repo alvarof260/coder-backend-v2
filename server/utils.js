6c171f8351a1ffa6a5644b58cecc07e30c6f65f9
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-01-11 17:51:09 -0300

    feat: add a new dependency bcrypt and create function helpers

--- server/utils.js
+++ server/utils.js
@@ -1,8 +1,13 @@
 import { fileURLToPath } from 'node:url'
 import { dirname } from 'node:path'
+import bcrypt from 'bcrypt'
 
 const __filename = fileURLToPath(import.meta.url)
 export const __dirname = dirname(__filename)
+
+export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password)
 
 export const verifyProduct = (product) => {
   const message = {}

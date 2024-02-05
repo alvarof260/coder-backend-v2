989255eee703cf90041a51033233d2458481aded
alvarof260 <alvarof260@gmail.com>
2024-02-05 13:22:12 -0300

    feat: Add support for uploading user documents and change user role to premium and vice versa

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,10 +4,29 @@ export const changeRoleController = async (req, res) => {
   try {
     const { uid } = req.params
     const user = await UserServices.getById(uid)
-    if (user === null) res.status(404).json({ status: 'error', error: 'user not found' })
-    user.role === 'user' ? user.role = 'premium' : user.role = 'user'
+    if (user === null) { res.status(404).json({ status: 'error', error: 'user not found' }) }
+    user.role === 'user' ? (user.role = 'premium') : (user.role = 'user')
     await UserServices.update(uid, user)
     res.status(200).json({ status: 'success', payload: user })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+export const uploadDocumentsController = async (req, res) => {
+  try {
+    const { uid } = req.params
+    const userUpdates = req.files.map(async (file) => {
+      await UserServices.update(uid, {
+        $push: {
+          document: {
+            name: file.originalname,
+            url: file.path
+          }
+        }
+      })
+    })
+    res.status(200).json({ status: 'success', payload: userUpdates })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
